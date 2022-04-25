769ee78d2fa6a3db12a6af57359e7dbb6d8637d0
lucas <lucas.sidnei32@gmail.com>
2022-04-25 20:09:19 -0300

    OK

--- run_testes.sh
+++ run_testes.sh
@@ -8,9 +8,7 @@ do
 	echo "-- Inicio do processo para p$folderCount --"
 	for i in ${!groups[@]};
 	do
-		echo "$i"
-		echo "${groups[$i]}"
-		if [[ ${groups[$i]}=="L3" ]]; then
+		if [[ ${groups[$i]} == "L3" ]]; then
 			echo "=== Inicio do processo de L3 ==="
 			for N in $sizes
 			do
@@ -18,30 +16,26 @@ do
 				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/l3/$N.txt
 				echo "[+] Finalizando $N.txt"
 			done
-		else
-			if [[ ${groups[$i]} == "L2CACHE" ]]; then
-				echo "=== Inicio do processo L2CACHE ==="
-				for N in $sizes
-				do
-					echo "[+] Inicializando $N.txt"
-					likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/l2cache/$N.txt
-					echo "[+] Finalizando $N.txt"
-				done
-			else
-				if [[ ${groups[$i]} == "FLOPS_DP" ]]; then
-					echo "=== Inici do processo FLOPS_DP ==="
-					for N in $sizes
-					do
-						echo "[+] Inicializando $N.txt"
-						likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/flops/$N.txt
-						echo "[+] Finalizando $N.txt"
-					done
-				fi
-			fi
 		fi
-
-
-		
+		if [[ ${groups[$i]} == "L2CACHE" ]]; then
+			echo "=== Inicio do processo L2CACHE ==="
+			for N in $sizes;
+			do
+				echo "[+] Inicializando $N.txt"
+				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/l2cache/$N.txt
+				echo "[+] Finalizando $N.txt"
+			done
+		fi
+		if [[ ${groups[$i]} == "FLOPS_DP" ]]; then
+			echo "=== Inici do processo FLOPS_DP ==="
+			for N in $sizes
+			do
+				echo "[+] Inicializando $N.txt"
+				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/flops/$N.txt
+				echo "[+] Finalizando $N.txt"
+			done
+		fi	
+				
 	done
 done
 

dd389d7d98aca19bd3853fd2bd8eff973fd9df20
lucas <lucas.sidnei32@gmail.com>
2022-04-25 20:23:15 -0300

    OK

--- run_testes.sh
+++ run_testes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sizes=(10)
+sizes=(10 32 50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096)
 groups=("L3" "L2CACHE" "FLOPS_DP")
 
 for folderCount in 2
@@ -10,29 +10,29 @@ do
 	do
 		if [[ ${groups[$i]} == "L3" ]]; then
 			echo "=== Inicio do processo de L3 ==="
-			for N in $sizes
+			for N in ${!sizes[@]};
 			do
-				echo "[+] Inicialiazando $N.txt"
-				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/l3/$N.txt
-				echo "[+] Finalizando $N.txt"
+				echo "[+] Inicialiazando ${sizes[$N]}.txt"
+				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/${sizes[$N]}.txt > ./p$folderCount/l3/${sizes[$N]}-l3.txt
+				echo "[+] Finalizando ${sizes[$N]}.txt"
 			done
 		fi
 		if [[ ${groups[$i]} == "L2CACHE" ]]; then
 			echo "=== Inicio do processo L2CACHE ==="
-			for N in $sizes;
+			for N in ${!sizes[@]};
 			do
-				echo "[+] Inicializando $N.txt"
-				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/l2cache/$N.txt
-				echo "[+] Finalizando $N.txt"
+				echo "[+] Inicializando ${sizes[$N]}.txt"
+				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/${sizes[$N]}.txt > ./p$folderCount/l2cache/${sizes[$N]}-l2cache.txt
+				echo "[+] Finalizando ${sizes[$N]}.txt"
 			done
 		fi
 		if [[ ${groups[$i]} == "FLOPS_DP" ]]; then
 			echo "=== Inici do processo FLOPS_DP ==="
-			for N in $sizes
+			for N in ${!sizes[@]}
 			do
-				echo "[+] Inicializando $N.txt"
-				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/$N.txt > ./p$folderCount/flops/$N.txt
-				echo "[+] Finalizando $N.txt"
+				echo "[+] Inicializando ${sizes[$N]}.txt"
+				likwid-perfctr -c 3 -g ${groups[$i]} ./p$folderCount/newtonPC < ./in/${sizes[$N]}.txt > ./p$folderCount/flops/${sizes[$N]}-flops.txt
+				echo "[+] Finalizando ${sizes[$N]}.txt"
 			done
 		fi	
 				
